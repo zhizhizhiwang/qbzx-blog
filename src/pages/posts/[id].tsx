e7c7217355275a7b202d880efa53db78b48c0e2f
zhizhizhiwang <1073171253@qq.com>
2025-04-26 13:13:57 +0800

    居然本地能构建

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,15 +1,22 @@
 import Title from "@/item/title";
-import { useRouter } from 'next/router'
+import { GetStaticProps, GetStaticPaths } from "next";
 import Sidebar from "@/item/sidebar";
 import styles from "@/css/page.module.css"
+import matter from 'gray-matter';
 
+interface PostData {
+    [key: string]: any; // frontmatter 字段，如 title, date 等
+}
 
-export const runtime = "edge";
+interface TestPageProps {
+    id: string;
+    content: string;
+    data: PostData;
+}
 
-export default function TestPage({ params }) {
-    const router = useRouter()
-    const id = router.query.id as string; // 获取动态路由参数
+// export const runtime = "edge";
 
+export default function TestPage({ id, content, data }: TestPageProps) {
     return (
         <div>
             <div className={styles.container}>
@@ -21,9 +28,56 @@ export default function TestPage({ params }) {
                     hrefs={["/posts", "/about"]}
                 />
                 <div className={styles.content}>
-                    
+                    <>
+                        <Title text={id} />
+                        {JSON.stringify(data)}
+                    </>
                 </div>
             </div>
         </div>
     );
 }
+
+export const getStaticProps: GetStaticProps<TestPageProps> = async ({ params }) => {
+    const id = params?.id as string;
+
+    const fs  = await require('fs');
+    const path = await require('path');
+    const fullPath = path.join('public', 'posts', `${id}.md`);
+
+    const fileExists = fs.existsSync(fullPath);
+    if (!fileExists) {
+        throw new Error(`File not found: ${fullPath}`);
+    }
+
+    let fileContents = fs.readFileSync(fullPath, 'utf8');
+    const matterResult = matter(fileContents);
+
+    return {
+        props: {
+            id: id,
+            data: matterResult.data,
+            content: matterResult.content,
+        },
+    };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const fs = await require('fs');
+    const path = await require('path');
+    const postsDirectory = path.join('public', 'posts');
+    const filenames = fs.readdirSync(postsDirectory);
+
+    const paths = filenames.map((filename: string) => {
+        return {
+            params: {
+                id: filename.replace(/\.md$/, ''),
+            },
+        };
+    });
+    
+    return {
+        paths,
+        fallback: false,
+    };
+};
