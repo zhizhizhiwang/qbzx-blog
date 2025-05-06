a0f38a82a6cb24883f20d11dc95ad348a4bce4e1
zhizhizhiwang <1073171253@qq.com>
2025-05-06 16:16:02 +0800

    登录页补丁

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Sidebar from '@/item/sidebar';
 import {
     SignInButton,
     SignUpButton,
@@ -7,34 +8,44 @@ import {
     UserButton,
 } from '@clerk/nextjs'
 import styles from "@/css/login.module.css";
+import pagestyles from "@/css/page.module.css"
 
 export const runtime = 'edge'
 
 export default function Login() {
     return (
-        <div className={styles.container}>
-            <div className={styles.loginBox}>
-                <h1 className={styles.title}>欢迎回来</h1>
-                <SignedOut>
-                    <div className={styles.subtitle}>登录以继续访问</div>
-                    <div className={styles.buttonContainer}>
-                        <SignInButton mode="modal">
-                            <button className={`${styles.button} ${styles.loginButton}`}>
-                                登录
-                            </button>
-                        </SignInButton>
-                        <SignUpButton mode="modal">
-                            <button className={`${styles.button} ${styles.registerButton}`}>
-                                注册
-                            </button>
-                        </SignUpButton>
-                    </div>
-                </SignedOut>
-                <SignedIn>
-                    <div>
-                        <UserButton />
-                    </div>
-                </SignedIn>
+        <div className={`${styles.container}`}>
+            <div  className={styles.position_fixed} >
+                <Sidebar 
+                    page="登录页面"
+                    title="登录页面"
+                    author={["zhizhizhiwang"]}
+                />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.loginBox}>
+                    <h1 className={styles.title}>欢迎回来</h1>
+                    <SignedOut>
+                        <div className={styles.subtitle}>登录以继续访问</div>
+                        <div className={styles.buttonContainer}>
+                            <SignInButton mode="modal">
+                                <button className={`${styles.button} ${styles.loginButton}`}>
+                                    登录
+                                </button>
+                            </SignInButton>
+                            <SignUpButton mode="modal">
+                                <button className={`${styles.button} ${styles.registerButton}`}>
+                                    注册
+                                </button>
+                            </SignUpButton>
+                        </div>
+                    </SignedOut>
+                    <SignedIn>
+                        <div>
+                            <UserButton />
+                        </div>
+                    </SignedIn>
+                </div>
             </div>
         </div>
     )
