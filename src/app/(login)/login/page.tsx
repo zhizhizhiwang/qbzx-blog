820213cd51bdacf4ed7dd11952aea86b5b1e712d
zhizhizhiwang <1073171253@qq.com>
2025-05-06 12:19:52 +0800

    测试

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -16,9 +16,10 @@ export default function Login() {
         <div className={styles.container}>
             <div className={styles.loginBox}>
                 <h1 className={styles.title}>欢迎回来</h1>
+                <SignedOut>
                 <div className={styles.subtitle}>登录以继续访问</div>
                 <div className={styles.buttonContainer}>
-                    <SignedOut>
+                    
                         <SignInButton mode="modal">
                             <button className={`${styles.button} ${styles.loginButton}`}>
                                 登录
@@ -29,11 +30,13 @@ export default function Login() {
                                 注册
                             </button>
                         </SignUpButton>
-                    </SignedOut>
-                    <SignedIn>
-                        <UserButton />
-                    </SignedIn>
                 </div>
+                </SignedOut>
+                <SignedIn>
+                    <div>
+                        <UserButton />
+                    </div>
+                </SignedIn>
             </div>
             
         </div>
