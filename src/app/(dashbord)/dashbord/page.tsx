34757226ab03de2a563af2b1d8311b0c235baca1
zhizhizhiwang <1073171253@qq.com>
2025-05-18 23:10:45 +0800

    文章页预览

---
+++ src/app/(dashbord)/dashbord/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import styles from "@/css/page.module.css";
+import Footer from "@/item/footer";
+import Title from "@/item/title";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+
+      <Title text="QBBS" subtitle="文章页" />
+      我知道你很急, 但是你先别急
+      
+    </>
+  );
+}
